fd9cef1c708ac76644bf39c61f2a510897515b4d
Aleksandr Barmin <abarmin@outlook.com>
2022-04-24 17:15:28 +0100

    Changing a little to use my own empty artefact

--- README.md
+++ README.md
@@ -3,7 +3,8 @@
 The following commands are added:
 
 - `mvn-archetype-generate-empty` - create an empty Apache Maven project
-- `mvn-wrapper-add` - add Apache Maven wrappper
+- `mvn-wrapper-add` - add Apache Maven wrapper
+- `mvn-archetype-new` - create a new Apache Maven archetype
 
 ## Installation
 
--- mac/zsh/maven.sh
+++ mac/zsh/maven.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
 mvn-archetype-generate-empty() {
-    mvn archetype:generate -DarchetypeArtifactId=maven-archetype-quickstart -DarchetypeVersion=1.4
+    mvn archetype:generate \
+        -DarchetypeArtifactId=archetype-empty \
+        -DarchetypeGroupId=dev.abarmin.archetype
+}
+
+mvn-archetype-new() {
+    mvn archetype:generate -DarchetypeGroupId=org.apache.maven.archetypes -DarchetypeArtifactId=maven-archetype-archetype
 }
 
 mvn-wrapper-add() {
