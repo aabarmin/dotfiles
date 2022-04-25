3cc772addae49f5a2dfa2521836aada52311b648
Aleksandr Barmin <abarmin@outlook.com>
2022-04-25 14:10:20 +0100

    Replace functions with aliases
    Closes #1

--- mac/zsh/maven.sh
+++ mac/zsh/maven.sh
@@ -1,15 +1,5 @@
 #!/bin/bash
 
-mvn-archetype-generate-empty() {
-    mvn archetype:generate \
-        -DarchetypeArtifactId=archetype-empty \
-        -DarchetypeGroupId=dev.abarmin.archetype
-}
-
-mvn-archetype-new() {
-    mvn archetype:generate -DarchetypeGroupId=org.apache.maven.archetypes -DarchetypeArtifactId=maven-archetype-archetype
-}
-
-mvn-wrapper-add() {
-    mvn wrapper:wrapper
-}
+alias mvn-wrapper-add='mvn wrapper:wrapper'
+alias mvn-archetype-new='mvn archetype:generate -DarchetypeGroupId=org.apache.maven.archetypes -DarchetypeArtifactId=maven-archetype-archetype'
+alias mvn-archetype-generate-empty='mvn archetype:generate -DarchetypeArtifactId=archetype-empty -DarchetypeGroupId=dev.abarmin.archetype'
